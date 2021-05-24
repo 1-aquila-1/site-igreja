3db34ecffcc4d74381eeeeebdfce1326567d2c87
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-23 22:51:25 -0300

    Arquivo de teste AdPedraAngularApplication

---
+++ ad-pedra-angular/src/test/java/br/com/adpedraangular/AdPedraAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adpedraangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdPedraAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
