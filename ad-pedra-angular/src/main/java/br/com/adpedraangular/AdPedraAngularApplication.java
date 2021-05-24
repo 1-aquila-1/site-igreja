b4312c5b6f65c844f8ff73be44148154d5eb5aa6
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-23 22:50:45 -0300

    Classe de aplicação

---
+++ ad-pedra-angular/src/main/java/br/com/adpedraangular/AdPedraAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.adpedraangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdPedraAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdPedraAngularApplication.class, args);
+	}
+
+}
